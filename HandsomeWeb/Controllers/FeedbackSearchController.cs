19013c067d65298b32b3776be0f9a25dfb8b1764
hyojeon <hyojeon@ebay.com>
2016-12-08 04:28:09 +0900

    modofy

---
+++ HandsomeWeb/Controllers/FeedbackSearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Feedback.ContentsFeeder;
+using Feedback.ContentsFeeder.Converter;
+using Feedback.ContentsFeeder.BizDac;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using MyTaskListApp.Models;
+using HandsomeWeb.Models;
+
+namespace HandsomeWeb.Controllers
+{
+	public class FeedbackSearchController : Controller
+	{
+
+		public ActionResult Index()
+		{
+
+			return View();
+		}
+	}
+}
