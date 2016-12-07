04a80e772b09528809584d94ac5f58bb864a18a0
Park Suhyun <suhpark@ebay.com>
2016-12-08 04:24:44 +0900

    Below PAge

--- HandsomeWeb/Controllers/FeedbackController.cs
+++ HandsomeWeb/Controllers/FeedbackController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Util;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
 using Feedback.ContentsFeeder;
 using Feedback.ContentsFeeder.Converter;
 using Feedback.ContentsFeeder.BizDac;
@@ -10,6 +16,9 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 using MyTaskListApp.Models;
+using HandsomeWeb.Models;
+using Feedback.ContentsFeeder.AzureWebservice;
+
 using HandsomeWeb.Models;
 
 namespace HandsomeWeb.Controllers
@@ -51,12 +60,43 @@ namespace HandsomeWeb.Controllers
             return View();
         }
 
+        public ActionResult RealFeedbackAnalyzeStep2(string orderNo)
+        {
+            string prvwNo = orderNo;
+            string url = String.Format("http://bamboo.gmarket.co.kr/Feedback/Premium/GetPremiumFeedbackDetail?prvwNo={0}", prvwNo);
+
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString(url);
+                var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+               // SomeModel model = serializer.Deserialize<SomeModel>(json);
+                // TODO: do something with the model
+            }
+
+            
+
+
+
+
+                // http://bamboo.auction.co.kr/Feedback/Feedback/GetFeedbackSecondDetail?orderno=1223317364
+
+
+                RealFeedbackSearchModel data = new RealFeedbackSearchModel();
+            data.Title = "좋아요~~";
+            data.Contents = "가격만족도/성능/사용편의성 등 : 가격도 저렴하고 좋아요^^ 상태/배송 : 배송도 빠릅니다~^^ 그 외 도움이 될 만한 사용후기 : 항상 핑크만 썼었는데 이번에 노란색 주문해서 썼어요~ 노란색 향도 좋아요~~";
+            data.ImageUrl = "http://img.iacstatic.co.kr/feedback/A/2016/12/5/f1098f87f26e45be8792297ebee166f7.jpg";
+
+            return View(data);
+        }
+
         public JsonResult GetRealFeedback(string orderno)
         {
             string returnString = "test";
 
 
+
             return Json(returnString);
+            // return RedirectToAction("RealFeedbackAnalyzeStep2", "Feedback");
         }
 
         [ValidateInput(false)]
@@ -79,6 +119,32 @@ namespace HandsomeWeb.Controllers
 
 			return Json(String.Format("당신의 점수는 {0}점입니다.\n당신의 글은 {1} 상품평입니다. \n상품평의 감성점수는 {2}점 입니다. {3} {4} {5}", test.FbQuality, sensitive, test.SensitiveScore, test.AnalysisedText, test.PositiveScore, test.NgativeScore), JsonRequestBehavior.AllowGet);
 		}
+
+        [ValidateInput(false)]
+        public JsonResult FeedbackParsingData(string Name1)
+        {
+            FeedbackDocument test = new FeedbackDocument(FbSite.Auction, String.Empty, 0, String.Empty, String.Empty, Name1, FbInputChannel.PC, DateTime.Now);
+
+            string sensitive = "긍정도 부정도 아닌";
+
+            if (test.SensitiveScore > 0)
+            {
+                sensitive = "긍정적인 평가의";
+            }
+            else if (test.SensitiveScore < 0)
+            {
+                sensitive = "부정적인 평가의 ";
+            }
+
+            //test.FbQuality; // 점수
+            //sensitive; // 상품평
+            //test.SensitiveScore; // 감성점수
+            //test.AnalysisedText; //
+            //test.PositiveScore; // 긍정점수
+            //test.NgativeScore; // 부정점수
+
+            return Json(String.Format("{0} / {1} / {2} / {3} / {4} / {5}", test.FbQuality, sensitive, test.SensitiveScore, test.AnalysisedText, test.PositiveScore, test.NgativeScore), JsonRequestBehavior.AllowGet);
+        }
 
         public JsonResult MongoDBTest(string Name1)
         {
--- HandsomeWeb/Scripts/Feedback/realFeedbackSearch.js
+++ HandsomeWeb/Scripts/Feedback/realFeedbackSearch.js
@@ -1,10 +1,10 @@
 ﻿$(window).load(function () {
-    realFeedbackAnalyze.init();
+    // realFeedbackAnalyze.init();
 });
 
 function realFeedbackAnalyze() { }
 
-realFeedbackAnalyze.init = function(){
+realFeedbackAnalyze.init = function () {
     $('#FeedbackGo').on('click', function () {
 
         var feedback = $('#targetFeedback').val();
@@ -52,8 +52,8 @@ realFeedbackAnalyze.init = function(){
         });
     });
 
-    // $('#searchFeedbackReal').on('click', function () {
-    //$('#searchFeedbackReal').click(function(){
+    //$('#searchFeedbackReal').on('click', function () {
+    //// $('#searchFeedbackReal').click(function(){
     //    var feedbackNo = $('#inputFeedbackNo').val();
 
     //    var param = [
@@ -69,8 +69,9 @@ realFeedbackAnalyze.init = function(){
     //        cache: false,
     //        success: function (data) {
     //            var a = 1;
+    //            alert(112);
     //            // $("#realFeedbackTtile").value(data);
-    //            $(".caption > h3").html("TesT")
+    //            // $(".caption > h3").html("TesT")
     //        },
     //        error: function (result) {
     //            alert("Fail");
@@ -81,5 +82,30 @@ realFeedbackAnalyze.init = function(){
 
 function feedbackEvent() { }
 feedbackEvent.searchFeedbackReal = function () {
-    $(".caption > h3").html("TesT")
+    // $(".caption > h3").html("TesT");
+    // $('#realFeedback').append('<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src="http://img.iacstatic.co.kr/feedback/A/2016/12/5/f1098f87f26e45be8792297ebee166f7.jpg" alt="..."><div class="caption"><h3>좋아요~~</h3><p>가격만족도/성능/사용편의성 등 : 가격도 저렴하고 좋아요^^ 상태/배송 : 배송도 빠릅니다~^^ 그 외 도움이 될 만한 사용후기 : 항상 핑크만 썼었는데 이번에 노란색 주문해서 썼어요~ 노란색 향도 좋아요~</p><p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p></div></div></div></div>');
+
+    var feedbackNo = $('#inputFeedbackNo').val();
+
+        var param = [
+            { name: 'orderno', value: feedbackNo }
+        ];
+
+        $.ajax({
+            url: "../Feedback/GetRealFeedback",
+            data: param,
+            type: 'POST',
+            dataType: "json",
+            async: false,
+            cache: false,
+            success: function (data) {
+                var a = 1;
+                alert(112);
+                // $("#realFeedbackTtile").value(data);
+                // $(".caption > h3").html("TesT")
+            },
+            error: function (result) {
+                alert("Fail");
+            }
+        });     
 };
