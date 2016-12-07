68040989a40301f973b1db67db9f64b313fcc16e
hyojeon <hyojeon@ebay.com>
2016-12-07 12:13:04 +0900

    modify

--- Feedback.ContentsFeeder/BizDac/FeedbackCsvT.cs
+++ Feedback.ContentsFeeder/BizDac/FeedbackCsvT.cs
@@ -8,6 +8,8 @@ namespace Feedback.ContentsFeeder.BizDac
 {
 	public class FeedbackCsvT
 	{
+		public string Title { get; set; }
+		public string OriginHtmlContents { get; set; }
 		public string Contents { get; set; }
 		public string AnaysisedContents { get; set; }
 		public int SiteId { get; set; }
@@ -19,5 +21,16 @@ namespace Feedback.ContentsFeeder.BizDac
 		public double PositiveScore { get; set; }
 		public double NgativeScore { get; set; }
 		public double SensitiveScore { get; set; }
+		public string ImageUrl1 { get; set; }
+		public string ImageUrl2 { get; set; }
+		public string ImageUrl3 { get; set; }
+		public string ImageUrl4 { get; set; }
+		public string ImageUrl5 { get; set; }
+		public string ImageUrl6 { get; set; }
+		public string ImageUrl7 { get; set; }
+		public string ImageUrl8 { get; set; }
+		public string ImageUrl9 { get; set; }
+		public string ImageUrl10 { get; set; }
+		public DateTime FbData { get; set; }
 	}
 }
